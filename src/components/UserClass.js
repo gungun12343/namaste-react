d6275333aa4ba7b9cb2ef5a37ffb442433492573
Gungun Koshta <koshtagungun795@gmail.com>
2025-05-03 16:44:04 +0530

    class new changes

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,25 +4,31 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0,
+            userInfo: {
+                // name: "Dummy",
+                // location: "Mumbai"
+            }
         }
     }
 
-    render() {
-        const {name, location} = this.props;
-        const {count} = this.state;
+    async componentDidMount() {
+        const data = await fetch("https://api.github.com/users/akshaymarch7");
+        const json = await data.json();
+        console.log(json);
+        this.setState({
+            userInfo: json,
+        })
+    }
 
+    render() {
+        const {name, location, login, avatar_url} = this.state.userInfo;
+        
         return (
             <div className="user-card">
-                <h2>Count: {count} </h2>
-                <button onClick={() => {
-                    this.setState({
-                        count: this.state.count + 1,
-                    });
-                }} >Increase Count</button>
+                <img src={avatar_url} />
                 <h2>Name: {name}</h2>
                 <h3>Location: {location} </h3>
-                <h3>Contact: surbhi1234@gmail.com</h3>
+                <h3>Contact: @{login}</h3>
             </div>
         )
     }
